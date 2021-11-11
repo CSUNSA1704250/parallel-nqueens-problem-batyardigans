17507e36d450d4f65e7261d658f010d4a4b06a5e
Leslym03 <leslymita03@gmail.com>
2021-11-10 19:58:24 -0500

    finish

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <bits/stdc++.h>
 #include <omp.h>
-
+#include <iostream>
 
 using namespace std;
 
+bool found = false;
+
 long long int ans = 0;
 
-
 int  SIZE, MASK;
+
 ofstream file("files/solutions.txt");
 
 string allsolutions = "";
+
+// ------------all----------
+
 void printtable(int left,int right, int SIZE)
 {   
     std::string lef = std::bitset< 32 >(left ).to_string(); // string conversion
@@ -83,28 +88,93 @@ void divideTask(int n){
 
 }
 
+// ------------find----------
+
+void print_find(int queens[], int SIZE){
+    ofstream file("files/solutions.txt");
+    file << "#Solutions for "<< SIZE << " queens\n\n";
+    file << "1 ";
+    for(int row=0; row<SIZE; row++) {
+      file << queens[row];
+      file << " ";
+    }
+    file << "\n";
+    file.close();
+}
+
+void task_find(int queens[], int row, int col, int id, int SIZE){
+    for(int i=0; i<row; i++){
+        if (queens[i]==col) 
+            return;
+        if (abs(queens[i]-col) == (row-i))
+            return;
+    }
+    queens[row]=col;
+    if(found)
+        return;
+    if(row == SIZE-1){
+        found = true;
+        #pragma omp critical
+        {
+            print_find(queens, SIZE);
+        }
+        return;
+    }
+    else{
+        for(int i=0; i<SIZE; i++){
+            task_find(queens, row+1, i, id, SIZE);
+        }
+    }
+}
+
+void queen_find(int SIZE){
+    int myid=0;
+    #pragma omp parallel
+    #pragma omp single
+    {
+        for(int i=0; i<SIZE; i++){
+            #pragma omp task
+            task_find(new int[SIZE], 0, i, myid, SIZE);
+        }
+    }
+}
+
+// ------------main----------
+
 int main(int argc, char** argv){
     int queens = stoi(argv[2]);
-    
+    string problemType = argv[1];
+
     if(argc != 3)
     {
         std::cout << "ERRROR ARGS::"<< argv[0]<<"<type [all, find]> <number_of_queens> \n";
         exit(1);
     }
 
-    auto begin = std::chrono::high_resolution_clock::now(); 
-    divideTask(queens);
-    std::chrono::duration<double> elapsed = std::chrono::system_clock::now() - begin;
+    if(problemType == "all"){
+        auto begin = std::chrono::high_resolution_clock::now(); 
+        divideTask(queens);
+        std::chrono::duration<double> elapsed = std::chrono::system_clock::now() - begin;
 
         // Print board size, number of solutions, and execution time. 
-    std::cout << "Board Size: " << queens << std::endl; 
-    std::cout << "Number of solutions: " << ans << std::endl; 
-    std::cout << "Execution time: "  << std::fixed << std::setprecision(9) << elapsed.count() << " seconds." <<std::endl;
-        
-    file << "#Solutions for "<<queens<<" queens"<<std::endl; 
-    file << ans << std::endl; 
+        std::cout << "Board Size: " << queens << std::endl; 
+        std::cout << "Number of solutions: " << ans << std::endl; 
+        std::cout << "Execution time: "  << std::fixed << std::setprecision(9) << elapsed.count() << " seconds." <<std::endl;
+            
+        file << "#Solutions for "<<queens<<" queens"<<std::endl; 
+        file << ans << std::endl; 
 
-    file << allsolutions;
-    file.close();
+        file << allsolutions;
+        file.close();
+    }
+
+    if(problemType == "find"){
+        auto begin = std::chrono::high_resolution_clock::now(); 
+        queen_find(queens);
+        std::chrono::duration<double> elapsed = std::chrono::system_clock::now() - begin;
+        
+        std::cout << "Execution time: "  << std::fixed << std::setprecision(9) << elapsed.count() << " seconds." <<std::endl;
+    }
+
     return 0;
 }
